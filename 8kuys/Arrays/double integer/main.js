be8f64128b273f6a5509f6a6310210dd6c94b33a
choco-develop <onoigboriaose@gmail.com>
2022-05-13 12:56:27 +0100

    doubleinteger

---
+++ 8kuys/Arrays/double integer/main.js
@@ -0,0 +1,7 @@
+//code as fast as you can,you need to double the integer and rreturn it
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+     i+=i
+    return i;
+  }
