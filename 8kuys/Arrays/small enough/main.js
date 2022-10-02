2f44b95ca870abe69335143cb30811120041d66d
choco-develop <onoigboriaose@gmail.com>
2022-10-02 20:00:00 +0100

    arrayyy

--- 8kuys/Arrays/small enough/main.js
+++ 8kuys/Arrays/small enough/main.js
@@ -8,3 +8,10 @@ function smallEnough(a, limit){
     let b= a.filter((x)=>x<=limit)
     return a.length===b.length? true:false
     }
+
+    //every is a js method that returns a boolean.
+    //it tests whether the e3lements in the array meets the conditions specified in the function
+
+    function smallEnough(a, limit){
+        return a.every(x => x <= limit);
+      }
