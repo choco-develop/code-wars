cc0c3c9f72861b02175ea65a08cfb74c3e8a3af1
choco-develop <onoigboriaose@gmail.com>
2022-10-08 20:05:58 +0100

    even-od

---
+++ 8kuys/Arrays/even or odd/m,ain.js
@@ -0,0 +1,7 @@
+function simpleMultiplication(n) {
+    // your code........
+  return n%2===0?n*8:n*9
+}
+
+
+/*This kata is about multiplying a given number by eight if it is an even number and by nine otherwise. */
