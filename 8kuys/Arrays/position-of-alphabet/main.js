9bddc801a1f3e195c045c60ebfc73db07cc2dcf9
choco-develop <onoigboriaose@gmail.com>
2022-10-31 11:49:08 +0100

    position-of-alphabet

--- 8kuys/Arrays/position-of-alphabet/main.js
+++ 8kuys/Arrays/position-of-alphabet/main.js
@@ -9,12 +9,11 @@ Ouput :: "Position of alphabet: 1"
 function position(letter){
     //Write your own Code!
       let index=0
-    let alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","s","y","z"]
+    let alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
      alphabet.forEach((x,y)=>{
-      if (letter==x){
+      if (letter===x){
         index=y+1
       }
     })
       return `Position of alphabet: ${index}`
     }
-    
