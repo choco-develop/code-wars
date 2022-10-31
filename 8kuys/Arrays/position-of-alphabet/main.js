6587203370884b4411c5f43dfcab815013da652f
choco-develop <onoigboriaose@gmail.com>
2022-10-31 11:50:35 +0100

    position-of-alphabet

--- 8kuys/Arrays/position-of-alphabet/main.js
+++ 8kuys/Arrays/position-of-alphabet/main.js
@@ -16,4 +16,14 @@ function position(letter){
       }
     })
       return `Position of alphabet: ${index}`
+
     }
+
+
+
+    //alternate solution
+
+function position(letter){
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  return 'Position of alphabet: ' + (alphabet.indexOf(letter) + 1);
+}
