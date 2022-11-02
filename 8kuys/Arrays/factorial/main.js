8a71c094fac436cabf0fc9e3fade1191a0d08434
choco-develop <onoigboriaose@gmail.com>
2022-11-02 22:09:59 +0100

    factoriaL

---
+++ 8kuys/Arrays/factorial/main.js
@@ -0,0 +1,16 @@
+/**Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+
+ */
+
+
+function factorial(n){
+  let final=1
+  for(let i=n;i>0;i--){
+    final*=i
+  }
+  return final
+}
+ 
+
