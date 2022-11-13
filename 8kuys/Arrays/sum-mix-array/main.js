18ef97074a67e61a1e7d64d12996cbafe3a133b4
choco-develop <onoigboriaose@gmail.com>
2022-11-13 23:23:37 +0100

    mix-array-sum-using-reduce(+C)

--- 8kuys/Arrays/sum-mix-array/main.js
+++ 8kuys/Arrays/sum-mix-array/main.js
@@ -6,3 +6,4 @@ function sumMix(x){
     return x.reduce((a,c)=>a+(+c),0)
      
    }
+   /**How to convert a string to a number in JavaScript using the unary plus operator ( + ) The unary plus operator ( + ) will convert a string into a number. The operator will go before the operand. We can also use the unary plus operator ( + ) to convert a string into a floating point number. */
