41744ad6d8b2481729ede4744a451430a103abd1
choco-develop <onoigboriaose@gmail.com>
2022-11-14 22:03:26 +0100

    array-to-string-split

---
+++ 8kuys/Arrays/array-to-string/main.js
@@ -0,0 +1,18 @@
+/**Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+Javascript examples
+printArray([1,2,3]) // --> '1,2,3' : elements are Numbers
+printArray([[1,2],["a","b"]]) // --> '[[1,2],["a","b"]]'  */
+
+
+
+function printArray(array){
+    //show me the code!
+     return array.toString();
+  }
